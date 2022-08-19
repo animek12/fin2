5e57cfbfe56cb0da745865ab26e81dab4a25f7de
animek12 <111511065+animek12@users.noreply.github.com>
2022-08-19 14:21:13 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
+*ɴᴀᴍᴇ:* FIN
+*ᴀɢᴇ:* 17ᵗʰ
 *sᴛᴀᴛᴜs:* single 
 
-*ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ʙɪʀᴛʜᴅᴀʏ:* 15 JULY 2005
+*ᴀᴅᴅʀᴇss:* Indonesia, kalimantan, samarinda
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
